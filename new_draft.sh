bd70fc22c2c8399f9b0742180a1f073f3110acb1
Douglas Lovell <doug@wbreeze.com>
2020-10-04 15:37:08 -0300

    Correct location of drafts in new_draft script

--- new_draft.sh
+++ new_draft.sh
@@ -6,8 +6,8 @@ fi
 ymd=`date +%Y-%m-%d`
 title=$1
 name=`echo ${title} | sed -E "s/[^[:alnum:]]+/-/g"`
-dest="./_posts/drafts/${name}.md"
-template="./_posts/drafts/template.md"
+dest="./_drafts/${name}.md"
+template="./_drafts/template.md"
 subst="s/title: title/title: ${title}/;s/date: .+/date: ${ymd}/"
 sed -E "${subst}" "${template}" >"${dest}"
 echo "Created ${dest}"
