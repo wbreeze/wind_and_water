afae1ac794f344d5c458d21aaf3c1a8c39c39121
Douglas Lovell <doug@wbreeze.com>
2026-08-18 13:18:12 +0100

    Revisions to scripts

--- assets/files/2026/piCam/grabVideo.sh
+++ assets/files/2026/piCam/grabVideo.sh
@@ -3,21 +3,23 @@ DESTV="/home/pi-admin"
 TIMESTAMP=`env TZ='UTC' date +%Y%m%d%H%M`
 VID_DIR="${DESTV}/Videos/Popa"
 LOG="${VID_DIR}/${TIMESTAMP}.log"
+CONFIG="${DESTV}/grabConfig.txt"
 
 CAM_0="${VID_DIR}/Cam0"
 [ -d ${CAM_0} ] || mkdir -p "${CAM_0}"
 DEST="${CAM_0}/Popa${TIMESTAMP}0.mp4"
 echo "$DEST"
-rpicam-vid --camera 0 -t 15s --config grabConfig.txt -o "${DEST}" >"${LOG}" 2>&1
+#rpicam-vid -v --camera 0 -t 5s --config "${CONFIG}" -o "${DEST}"
+rpicam-vid --camera 0 -t 15s --config "${CONFIG}" -o "${DEST}" >"${LOG}" 2>&1
 
 CAM_1="${VID_DIR}/Cam1"
 [ -d ${CAM_1} ] || mkdir -p "${CAM_1}"
 DEST="${CAM_1}/Popa${TIMESTAMP}1.mp4"
 echo "$DEST"
-rpicam-vid --camera 1 -t 15s --config grabConfig.txt -o "${DEST}" >>"${LOG}" 2>&1
+rpicam-vid --camera 1 -t 15s --config "${CONFIG}" -o "${DEST}" >>"${LOG}" 2>&1
 
 echo +3300 | tee /sys/class/rtc/rtc0/wakealarm
 if [ ! -e "${VID_DIR}/keepalive" ]; then
-  sudo shutdown now
+  sudo shutdown -s now
 fi
 
